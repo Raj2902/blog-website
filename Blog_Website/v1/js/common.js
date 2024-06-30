b2756cd67dec5b238d1b5f397335246eda73f507
Raj2902 <rajagarwalgood@gmail.com>
2024-06-30 12:09:58 +0530

    Added Advance template

--- Blog_Website/v1/js/common.js
+++ Blog_Website/v1/js/common.js
@@ -11,11 +11,89 @@ function changeCert() {
 function temp_select() {
   let data = document.getElementById("temp").value;
   if (data === "advance") {
-    alert("we are working on this template...");
+    document.getElementById("stylesheet").href = "./css/advanceTemplate.css";
   } else if (data === "intermidiate") {
     document.getElementById("stylesheet").href =
       "./css/intermidiateTemplate.css";
   } else {
     document.getElementById("stylesheet").href = "./css/basicTemplate.css";
+  }
+}
+/*To show dropdown skills*/
+function showDropdown() {
+  let show = document.getElementById("skill-drop");
+  show.style.display = "block";
+}
+function hideDropdown() {
+  let show = document.getElementById("skill-drop");
+  show.style.display = "none";
+}
+/*To show dropdown certificates*/
+function showDropdown_cert() {
+  let show = document.getElementById("skill-drop-cert");
+  show.style.display = "block";
+}
+function hideDropdown_cert() {
+  let show = document.getElementById("skill-drop-cert");
+  show.style.display = "none";
+}
+document.addEventListener("click", function (event) {
+  const clickedSkillsDrop = document.getElementById("skill-drop");
+  const clickedSkillsDropId = document.getElementById("skill-drop-id");
+  const clickedCertDrop = document.getElementById("skill-drop-cert");
+  const clickedCertDropId = document.getElementById("cert-drop-id");
+  // Check if clicked element is not the div itself or any of its children
+  if (
+    !clickedSkillsDrop.contains(event.target) &&
+    !clickedSkillsDropId.contains(event.target)
+  ) {
+    let show = document.getElementById("skill-drop");
+    show.style.display = "none";
+  }
+  if (
+    !clickedCertDrop.contains(event.target) &&
+    !clickedCertDropId.contains(event.target)
+  ) {
+    let show = document.getElementById("skill-drop-cert");
+    show.style.display = "none";
+  }
+});
+function showOverlayHtml() {
+  let show = document.getElementById("text-over-img-html");
+  show.style.display = "block";
+}
+function showOverlayGit() {
+  let show = document.getElementById("text-over-img-git");
+  show.style.display = "block";
+}
+function showOverlayJS() {
+  let show = document.getElementById("text-over-img-JS");
+  show.style.display = "block";
+}
+function hideOverlayHtml() {
+  let show = document.getElementById("text-over-img-html");
+  show.style.display = "none";
+}
+function hideOverlayGit() {
+  let show = document.getElementById("text-over-img-git");
+  show.style.display = "none";
+}
+function hideOverlayJS() {
+  let show = document.getElementById("text-over-img-JS");
+  show.style.display = "none";
+}
+function changeCertHtml() {
+  let changeCert = document.getElementById("html-cert-html");
+  changeCert.src = "";
+}
+function changeCertGit() {
+  let changeCert = document.getElementById("github-cert");
+  let data = changeCert.getAttribute("data-git-cert");
+  if (data === "1") {
+    changeCert.src = "./images/git_testdome certificate.png";
+    changeCert.setAttribute("data-git-cert", "2");
+  } else {
+    changeCert.src = "./images/git_GreatLearning certificate.png";
+    changeCert.setAttribute("data-git-cert", "1");
   }
 }
